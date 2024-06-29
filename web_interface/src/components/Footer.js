db7e1a94f54aa0e13f6fb7ee69f7aa8b954c3da7
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-29 00:43:19 -0700

    Create Footer.js

---
+++ web_interface/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2024 GPU Optimizer for ML Models</p>
+        </footer>
+    );
+}
+
+export default Footer;
