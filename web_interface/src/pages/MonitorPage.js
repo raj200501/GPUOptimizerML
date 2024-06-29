8ee834e9e7efeeb840fb487f5af3b3c56160d8ac
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-29 00:44:48 -0700

    Create MonitorPage.js

---
+++ web_interface/src/pages/MonitorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ModelMonitor from '../components/ModelMonitor';
+
+function MonitorPage() {
+    return (
+        <div>
+            <h1>Monitor Your Models</h1>
+            <ModelMonitor />
+        </div>
+    );
+}
+
+export default MonitorPage;
