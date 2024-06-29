2de63433a9c8671881348e28d129a61d66f867f2
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-29 00:44:33 -0700

    Create UploadPage.js

---
+++ web_interface/src/pages/UploadPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ModelUpload from '../components/ModelUpload';
+
+function UploadPage() {
+    return (
+        <div>
+            <h1>Upload Your Model</h1>
+            <ModelUpload />
+        </div>
+    );
+}
+
+export default UploadPage;
