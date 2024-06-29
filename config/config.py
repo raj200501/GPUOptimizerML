550a6a08214a1b0738e47b12cb124cbe17097542
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-29 00:36:19 -0700

    Delete config directory

--- config/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://user:password@localhost/gpu_optimizer_ml')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')
-    AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
-    AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
